7cb931dd3d2f6443a3fbcdd3b0c7d41c5d45fbab
MAZE <remvze@proton.me>
2024-09-30 21:23:48 +0330

    feat: add supabase

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
+  adapter: vercel(),
   integrations: [react(), sitemap()],
+  output: 'server',
   site: 'https://katana.mvze.net',
 });
