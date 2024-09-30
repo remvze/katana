33182057f3b4e7cc439753e3042cfdc21adc83f5
MAZE <remvze@proton.me>
2024-09-30 22:41:01 +0330

    feat: add basic cloudflare turnstile

--- src/components/shorten-form/shorten-form.tsx
+++ src/components/shorten-form/shorten-form.tsx
@@ -1,15 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Turnstile from 'react-turnstile';
 
 export function ShortenForm() {
+  const siteKey = import.meta.env.PUBLIC_TURNSTILE_SITE_KEY;
+
+  const [mountTurnstile, setMountTurnstile] = useState(false);
+  const [token, setToken] = useState('');
+
+  useEffect(() => setMountTurnstile(true), []);
+
   const [url, setUrl] = useState('');
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (!url) return null;
+    if (!url || !token) return null;
 
     const response = await fetch('/api/shorten-url', {
-      body: JSON.stringify({ url }),
+      body: JSON.stringify({ token, url }),
       headers: { 'Content-Type': 'application/json' },
       method: 'POST',
     });
@@ -26,6 +34,10 @@ export function ShortenForm() {
   return (
     <form onSubmit={handleSubmit}>
       <input type="url" value={url} onChange={e => setUrl(e.target.value)} />
+
+      {mountTurnstile && (
+        <Turnstile sitekey={siteKey} onVerify={token => setToken(token)} />
+      )}
     </form>
   );
 }
--- src/pages/api/shorten-url.ts
+++ src/pages/api/shorten-url.ts
@@ -6,9 +6,37 @@ export const POST: APIRoute = async ({ request }) => {
   if (request.headers.get('Content-Type') !== 'application/json')
     return new Response(null, { status: 400 });
 
-  const { url } = await request.json();
+  const { token, url } = await request.json();
 
-  const slug = await createUrl(url);
+  if (!token) {
+    return new Response(
+      JSON.stringify({ error: 'No turnstile token found.' }),
+      { status: 400 },
+    );
+  }
 
-  return new Response(JSON.stringify({ slug }), { status: 200 });
+  const verificationUrl =
+    'https://challenges.cloudflare.com/turnstile/v0/siteverify';
+
+  const body = new URLSearchParams();
+
+  body.append('secret', import.meta.env.TURNSTILE_SECRET_KEY);
+  body.append('response', token);
+
+  const verificationResponse = await fetch(verificationUrl, {
+    body,
+    method: 'POST',
+  });
+
+  const verificationResult = await verificationResponse.json();
+
+  if (verificationResult.success) {
+    const slug = await createUrl(url);
+
+    return new Response(JSON.stringify({ slug }), { status: 200 });
+  } else {
+    return new Response(JSON.stringify({ error: 'Verification failed.' }), {
+      status: 400,
+    });
+  }
 };
