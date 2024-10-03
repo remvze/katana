712afa52c836ec58482217f2c5d6c837543c25ea
MAZE <remvze@proton.me>
2024-10-03 14:37:08 +0330

    feat: change from vercel to node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/serverless';
+import node from '@astrojs/node';
 
 export default defineConfig({
-  adapter: vercel(),
+  adapter: node({
+    mode: 'standalone',
+  }),
   integrations: [react(), sitemap()],
   output: 'server',
   site: 'https://katana.mvze.net',
