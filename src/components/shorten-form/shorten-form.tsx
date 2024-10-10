85c278dd155a688f3b7606853a07ad5f9f34b8f6
MAZE <remvze@proton.me>
2024-10-10 22:38:13 +0330

    style: minor changes

--- src/components/shorten-form/shorten-form.tsx
+++ src/components/shorten-form/shorten-form.tsx
@@ -102,13 +102,6 @@ export function ShortenForm() {
               value={password}
               onChange={e => setPassword(e.target.value)}
             />
-            <p>
-              Generate a secure password using{' '}
-              <a href="https://pswd.mvze.net/" rel="noreferrer" target="_blank">
-                PSWD
-              </a>
-              .
-            </p>
           </div>
 
           {mountTurnstile && (
