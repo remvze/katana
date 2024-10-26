12816d5724866b29bfbff066c6f4fc9ede56b223
MAZE <remvze@proton.me>
2024-10-26 13:01:57 +0330

    chore: remove log

--- src/pages/api/[...paths].ts
+++ src/pages/api/[...paths].ts
@@ -15,8 +15,6 @@ app.get('/health', c => c.json({ success: true }, 200));
 app.get('expire', async c => {
   const authorization = c.req.header('Authorization');
 
-  console.log({ authorization });
-
   if (authorization !== `Bearer ${import.meta.env.EXPIRATION_SECRET}`) {
     return c.json({ success: false }, 403);
   }
